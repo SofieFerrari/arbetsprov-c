865f59bf6c0b23ee0dd5ec6748ef505a3c72af88
SofieFerrari <sofiestrahl@gmail.com>
2024-10-21 15:32:55 +0200

    started working on server.js and gameModel.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+import express from "express"
+import cors from "cors"
+import expressListEndpoints from "express-list-endpoints";
+
+const port = process.env.PORT || 8080
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+//import gameRoutes here
+const gameRoutes = require("./routes/gameRoutes")
+app.use("/games")
+
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`)
+})
